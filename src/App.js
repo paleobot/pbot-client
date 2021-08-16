07b7a290dc039248d5041a045778df960b6e9628
Douglas Meredith <noisyflowers@douglasmeredith.net>
2021-08-16 15:07:09 -0700

    Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import logo from './PBOT-logo-transparent.png';
 import './App.css';
+import Query from './components/Query';
+import ApolloQuery from './components/ApolloQuery';
+import OTUQueryForm from './components/OTUQueryForm';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#bdbdbd"
+    },
+    secondary: {
+      main: "#66bb6a"
+    }
+  }
+});
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+       <OTUQueryForm />
+       <br />
+       <br />
+   </div>
+   </ThemeProvider>
   );
 }
 
