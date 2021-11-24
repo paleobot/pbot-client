66344ab123cf58ebf97994ba7993e827b130777e
Douglas Meredith <noisyflowers@douglasmeredith.net>
2021-11-23 21:07:37 -0700

    Refactored mutation activity into its own component. So far, only used for Person mutations but should be able to use this for all node types. I think.

---
+++ src/components/Mutator.js
@@ -0,0 +1,128 @@
+import React, { useEffect } from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+  useQuery,
+  useMutation,
+  gql,
+  useApolloClient
+} from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('PBOTMutationToken');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+//Create new client. This will be passed explicitly to useMutation, since we already have a 
+//client defined in the context from ApolloProvider. 
+//TODO: Look into loading the ApolloProvider client on the fly using something like the 
+//method described in https://github.com/apollographql/apollo-client/issues/2897.
+const mclient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+const Mutator = (props) => {
+//function Mutater(props) {
+    console.log("Mutater");
+    console.log(props);
+    
+    const qclient = useApolloClient();
+
+    let gQL;
+    gQL = props.mode === "edit" ?
+        gql`
+            mutation ($data: ${props.entity}Input!) {
+                CustomUpdate${props.entity}(data: $data) {
+                    ${props.entity.toLowerCase()}ID
+                }      
+            }
+        ` :
+        props.mode === "create" ?
+        gql`
+            mutation ($data: ${props.entity}Input!) {
+                CustomCreate${props.entity}(data: $data) {
+                    ${props.entity.toLowerCase()}ID
+                }      
+            }
+        ` :
+        props.mode === "delete" ?
+        gql`
+            mutation ($data: ${props.entity}Input!) {
+                CustomDelete${props.entity}(data: $data) {
+                    ${props.entity.toLowerCase()}ID
+                }      
+            }
+        ` :
+        '';
+
+    const [mutatePerson, { data, loading, error }] = useMutation(gQL, {variables: {data: props.params}, client: mclient});
+
+    //Apollo client mutations are a little weird. Rather than executing automatically on render, 
+    //the hook returns a function we have to manually execute, in this case addDescription.
+    //The idea is that this would be attached to a submit event, I guess, but that's not 
+    //how this current architecture works. Instead, I'm using the useEffect hook with the empty 
+    //array option that causes it to only execute once.
+    useEffect(() => {
+            mutatePerson().catch((err) => {
+                //Just eat it. The UI will get what it needs below through the error field defined on the hook.
+                console.log("catch");
+                console.log(err);
+            });
+    }, []);
+
+    if (loading) {
+        return <p>Loading...</p>;
+    } else if (error) {
+        console.log("ERROR!");
+        console.log(error);
+        return <p>Error: {error.message}</p>;
+    } else if (data) {
+        console.log(data);
+        
+        //Force reload of cache
+        qclient.resetStore();
+
+        const style = {textAlign: "left", width: "100%", margin: "auto", marginTop:"1em"};
+        return props.mode === "edit" ?
+        (
+            <div key={data[`CustomUpdate${props.entity}`][`${props.entity.toLowerCase()}ID`]} style={style}>
+                {data[`CustomUpdate${props.entity}`][`${props.entity.toLowerCase()}ID`]} <br />
+                <br />
+            </div>
+        ) :
+        props.mode === "create" ?
+        (
+            <div key={data[`CustomCreate${props.entity}`][`${props.entity.toLowerCase()}ID`]} style={style}>
+                {data[`CustomCreate${props.entity}`][`${props.entity.toLowerCase()}ID`]} <br />
+                <br />
+            </div>
+        ) :
+        props.mode === "delete" ?
+        (
+            <div key={data[`CustomDelete${props.entity}`][`${props.entity.toLowerCase()}ID`]} style={style}>
+                {data[`CustomDelete${props.entity}`][`${props.entity.toLowerCase()}ID`]} <br />
+                <br />
+            </div>
+        ) :
+        '';
+                
+    } else {
+        return (<div></div>); //gotta return something until addDescription runs
+    }
+
+}
+
+export default Mutator;
--- src/components/Person/PersonMutateResults.js
+++ src/components/Person/PersonMutateResults.js
@@ -1,136 +1,12 @@
 import React, { useEffect } from 'react';
-import { Button, Grid } from '@material-ui/core';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-  useQuery,
-  useMutation,
-  gql,
-  useApolloClient
-} from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('PBOTMutationToken');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    }
-  }
-});
-//Create new client. This will be passed explicitly to useMutation, since we already have a 
-//client defined in the context from ApolloProvider. 
-//TODO: Look into loading the ApolloProvider client on the fly using something like the 
-//method described in https://github.com/apollographql/apollo-client/issues/2897.
-const mclient = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
-});
-
-function PersonMutate(props) {
-    console.log("PersonMutate");
-    console.log(props);
-    
-    const qclient = useApolloClient();
-
-    let gQL;
-    gQL = props.mode === "edit" ?
-        gql`
-            mutation ($data: PersonInput!) {
-                CustomUpdatePerson(data: $data) {
-                    personID
-                }      
-            }
-        ` :
-        props.mode === "create" ?
-        gql`
-            mutation ($data: PersonInput!) {
-                CustomCreatePerson(data: $data) {
-                    personID
-                }      
-            }
-        ` :
-        props.mode === "delete" ?
-        gql`
-            mutation ($data: PersonInput!) {
-                CustomDeletePerson(data: $data) {
-                    personID
-                }      
-            }
-        ` :
-        '';
-
-    const [mutatePerson, { data, loading, error }] = useMutation(gQL, {variables: {data: props.params}, client: mclient});
-
-    //Apollo client mutations are a little weird. Rather than executing automatically on render, 
-    //the hook returns a function we have to manually execute, in this case addDescription.
-    //The idea is that this would be attached to a submit event, I guess, but that's not 
-    //how this current architecture works. Instead, I'm using the useEffect hook with the empty 
-    //array option that causes it to only execute once.
-    useEffect(() => {
-            mutatePerson().catch((err) => {
-                //Just eat it. The UI will get what it needs below through the error field defined on the hook.
-                console.log("catch");
-                console.log(err);
-            });
-    }, []);
-
-    if (loading) {
-        return <p>Loading...</p>;
-    } else if (error) {
-        console.log("ERROR!");
-        console.log(error);
-        return <p>Error: {error.message}</p>;
-    } else if (data) {
-        console.log(data);
-        
-        //Force reload of cache
-        qclient.resetStore();
-
-        const style = {textAlign: "left", width: "100%", margin: "auto", marginTop:"1em"};
-        return props.mode === "edit" ?
-        (
-            <div key={data.CustomUpdatePerson.personID} style={style}>
-                {data.CustomUpdatePerson.personID} <br />
-                <br />
-            </div>
-        ) :
-        props.mode === "create" ?
-        (
-            <div key={data.CustomCreatePerson.personID} style={style}>
-                {data.CustomCreatePerson.personID} <br />
-                <br />
-            </div>
-        ) :
-        props.mode === "delete" ?
-        (
-            <div key={data.CustomDeletePerson.personID} style={style}>
-                {data.CustomDeletePerson.personID} <br />
-                <br />
-            </div>
-        ) :
-        '';
-                
-    } else {
-        return (<div></div>); //gotta return something until addDescription runs
-    }
-
-}
+import Mutator from '../Mutator';
 
 const PersonMutateResults = ({queryParams, queryEntity}) => {
     console.log("PersonMutateResults");
     console.log(queryParams);
 
     let persons = queryEntity === "Person-mutate" ? (
-                    <PersonMutate 
+                    <Mutator 
                         params={{
                             personID: queryParams.person || null,
                             given: queryParams.given || null,
@@ -138,6 +14,7 @@ const PersonMutateResults = ({queryParams, queryEntity}) => {
                             email: queryParams.email || null,
                             orcid: queryParams.orcid || null,
                         }}
+                        entity="Person"
                         mode={queryParams.mode}
                     />
                 ) : 
