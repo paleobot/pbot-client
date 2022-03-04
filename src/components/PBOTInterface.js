141edf6a77d04c28c5c3ed5442ca34f54cc0af09
Douglas Meredith <noisyflowers@douglasmeredith.net>
2022-03-04 16:42:41 -0700

    Use authlink to keep track of login status on client

--- src/components/PBOTInterface.js
+++ src/components/PBOTInterface.js
@@ -7,14 +7,35 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
+  createHttpLink,
   useQuery,
-  gql
+  useMutation,
+  gql,
+  useApolloClient
 } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: '/graphql',
+});
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('PBOTMutationToken');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+//TODO: Look into loading the ApolloProvider client on the fly using something like the 
+//method described in https://github.com/apollographql/apollo-client/issues/2897.
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
+
 
 const PBOTInterface = ({setRotatePBOT}) => {
     const [selectedTab, setSelectedTab] = useState(0);
