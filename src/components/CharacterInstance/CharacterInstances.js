d937290f15d4ab71bea0d1e9ed1806360b33d107
Douglas Meredith <noisyflowers@douglasmeredith.net>
2022-04-25 15:35:07 -0700

    Sort CharacterInstances includes order

--- src/components/CharacterInstance/CharacterInstances.js
+++ src/components/CharacterInstance/CharacterInstances.js
@@ -1,21 +1,20 @@
 import React from 'react';
+import { alphabetize } from '../../util.js';
+
+const massage = cI => {
+    cI.sortName = `
+            ${cI.state.order !== null ? `${cI.state.order}` : ``}${cI.character.name}${cI.state.value !== null ? `${cI.state.value}` : `${cI.state.State.name}`}
+`.toUpperCase();
+return cI
+};
 
 function CharacterInstances(props) {
     //console.log("CharacterInstances");
     if (!props.characterInstances) return ''; //TODO: is this the best place to handle this?
     //console.log(props.characterInstances);
-    let characterInstances = [...props.characterInstances];
-    characterInstances.sort((a,b) => {
-        const nameA = a.character.name.toUpperCase(); 
-        const nameB = b.character.name.toUpperCase(); 
-        if (nameA < nameB) {
-            return -1;
-        }
-        if (nameA > nameB) {
-            return 1;
-        }
-        return 0;
-    });
+
+    let characterInstances = alphabetize([...props.characterInstances].map(cI => massage({...cI})), "sortName");
+    
     const style = {marginLeft:"4em"}
     return characterInstances.map(({pbotID, character, state}) => (
         <div key={pbotID}  style={style}>
--- src/components/Description/Descriptions.js
+++ src/components/Description/Descriptions.js
@@ -4,19 +4,8 @@ import { alphabetize } from '../../util.js';
 
 function Descriptions(props) {
     console.log("Descriptions");
-    let descriptions = [...props.descriptions];
-    descriptions.sort((a,b) => {
-        const nameA = a.name ? a.name.toUpperCase() : "z"; //"z" forces null names to end of list
-        const nameB = b.name ? b.name.toUpperCase() : "z"; 
-        if (nameA < nameB) {
-            return -1;
-        }
-        if (nameA > nameB) {
-            return 1;
-        }
-        return 0;
-    });
-
+    let descriptions = alphabetize([...props.descriptions], "name");
+    
     const style = {textAlign: "left", width: "100%", margin: "auto", marginTop:"1em"}
     const indent = {marginLeft:"2em"}
     const indent2 = {marginLeft:"4em"}
@@ -40,7 +29,7 @@ function Descriptions(props) {
                 <div>
                     <div style={indent}><b>references:</b></div>
                     {alphabetize([...references], "order").map(reference => (
-                        <div style={indent2}>{reference.Reference.title}, {reference.Reference.publisher}, {reference.Reference.year}</div>
+                        <div key={reference.Reference.pbotID} style={indent2}>{reference.Reference.title}, {reference.Reference.publisher}, {reference.Reference.year}</div>
                     ))}
                 </div>
             </div>
