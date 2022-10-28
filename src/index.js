a176836a5538ddb9665676d5e19ba748775c559d
Douglas Meredith <noisyflowers@douglasmeredith.net>
2022-10-28 16:28:58 -0700

    Add router definition. Move theming from App.js to hear so it covers all routes.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,60 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import OTUDirectQueryResults from './components/OTU/OTUDirectQueryResults';
+import { createTheme, ThemeProvider, StyledEngineProvider } from '@mui/material';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/otu/:otuid",
+    element: < OTUDirectQueryResults/>
+  }
+]);
+
+const theme = createTheme({
+  palette: {
+      primary: {
+      main: "#bdbdbd"
+      },
+      secondary: {
+      main: "#66bb6a"
+      }
+  },
+  components : {
+      MuiTextField: {
+          defaultProps: {
+              variant: "standard"
+          }
+      },
+      MuiCheckbox: {
+          defaultProps: {
+              color: "secondary"
+          }
+      },
+      MuiRadio: {
+          defaultProps: {
+              color: "secondary"
+          }
+      }
+  }
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+    <React.StrictMode>
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+                <RouterProvider router={router} />
+            </ThemeProvider>
+        </StyledEngineProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
