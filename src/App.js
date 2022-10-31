e040f29040d85e803d2a5c7a7f2754dc73067b34
Douglas Meredith <noisyflowers@douglasmeredith.net>
2022-10-31 16:10:09 -0700

    Removed superfluous OTU direct code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React, { useState } from 'react';
-//import logo from './logo.svg';
 import logo from './PBOT-logo-transparent.png';
 import './App.css';
-//import Query from './components/Query';
-//import ApolloQuery from './components/ApolloQuery';
-//import OTUQueryForm from './components/OTUQueryForm';
 import PBOTInterface from './components/PBOTInterface';
 import Footer from "./components/Footer";
 import OTUDirectQueryResults from './components/OTU/OTUDirectQueryResults';
-
-import {ApolloProvider} from "@apollo/client";
-import {client} from './ApolloClientSetup.js';
-import { AppBar, Typography } from '@mui/material';
 
 const PBOTIcon = ({rotatePBOT}) => {
     const rotate = rotatePBOT ? "rotateY(180deg)" : "rotateY(0)";
@@ -23,13 +15,6 @@ const PBOTIcon = ({rotatePBOT}) => {
 function App() {
     const [rotatePBOT, setRotatePBOT] = useState(true);
 
-    //Handle request for standalone OTU result
-    if ("/" !== window.location.pathname) {
-        return (
-                   <OTUDirectQueryResults/>
-        );
-    }
-    
     //localStorage.removeItem('PBOTMutationToken');
     return (
         <div className="App">
