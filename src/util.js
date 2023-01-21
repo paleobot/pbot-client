a7041cae38123f90597e25f1a6c30bda91587bef
Douglas Meredith <noisyflowers@douglasmeredith.net>
2023-01-20 17:02:23 -0700

    New sort routine allowing sort on two fields

--- src/util.js
+++ src/util.js
@@ -11,3 +11,26 @@ export const alphabetize = (list, sortField) => {
         return 0;
     });
 }
+
+export const sort = (list, sortField1, sortField2) => {
+    return list.sort((a,b) => {
+        //"z" forces null/blank names to end of list
+        const field1A = a[sortField1] ? a[sortField1].toString().toUpperCase() : "z"; 
+        const field1B = b[sortField1] ? b[sortField1].toString().toUpperCase() : "z"; 
+
+        const field2A = a[sortField2] ? a[sortField2].toString().toUpperCase() : "z"; 
+        const field2B = b[sortField2] ? b[sortField2].toString().toUpperCase() : "z"; 
+
+        if (field1A < field1B) {
+            return -1;
+        } else if (field1A > field1B) {
+            return 1;
+        } else if (field2A < field2B) {
+            return -1;
+        } else if (field2A > field2B) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+}
