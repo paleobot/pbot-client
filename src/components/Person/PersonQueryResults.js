d039346e01027dbc126faac851b28f6faf191e39
Douglas Meredith <noisyflowers@douglasmeredith.net>
2023-02-21 16:12:05 -0700

    Handle query/output for PersonSelect

--- src/components/Person/PersonQueryResults.js
+++ src/components/Person/PersonQueryResults.js
@@ -5,6 +5,7 @@ import {
 } from "@apollo/client";
 import { alphabetize } from '../../util.js';
 import {publicGroupID} from '../Group/GroupSelect.js';
+import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 
 function Persons(props) {
     console.log(props);
@@ -13,8 +14,8 @@ function Persons(props) {
     let filters = Object.fromEntries(Object.entries(props.filters).filter(([_, v]) => v ));
 
     const gQL = gql`
-            query ($pbotID: ID, $given: String, $surname: String, $email: String, $orcid: String, $groups: [ID!]) {
-                Person (pbotID: $pbotID, given: $given, surname: $surname, email: $email, orcid: $orcid, filter:{memberOf_some: {pbotID_in: $groups}}) {
+            query ($pbotID: ID, $given: String, $surname: String, $email: String, $orcid: String, $groups: [ID!], $excludeList: [ID!]) {
+                Person (pbotID: $pbotID, given: $given, surname: $surname, email: $email, orcid: $orcid, filter:{AND: [{memberOf_some: {pbotID_in: $groups}}, {pbotID_not_in: $excludeList}]}) {
                     pbotID
                     given
                     surname
@@ -23,10 +24,14 @@ function Persons(props) {
                 }
             }
         `;
-    
+
+    //For AuthorManager applications, omit persons that are already in the list
+    const excludeIDs = props.exclude ? props.exclude.map(person => person.pbotID) : [];
+
     const { loading, error, data } = useQuery(gQL, {
         variables: {
-            ...filters
+            ...filters,
+            excludeList: excludeIDs
         },
         fetchPolicy: "cache-and-network"
     });
@@ -38,6 +43,20 @@ function Persons(props) {
 
     const style = {textAlign: "left", width: "100%", margin: "auto", marginTop:"1em"}
     const indent = {marginLeft:"2em"}
+    if (props.select) {
+        return (
+            <List sx={{ pt: 0 }}>
+            {people.map((person) => (
+                <ListItem disableGutters key={person.pbotID}>
+                    <ListItemButton onClick={() => props.handleSelect(person)} >
+                        <ListItemText 
+                        primary={person.given + " " + person.surname} secondary={`pbot id: ${person.pbotID}`} />
+                    </ListItemButton>
+                </ListItem>
+            ))}
+        </List>
+        )
+    }
     return (people.length === 0) ? (
         <div style={style}>
             No {(filters.groups && filters.groups.length === 1 && publicGroupID === filters.groups[0]) ? "public" : ""} results were found.
@@ -54,7 +73,7 @@ function Persons(props) {
 
 }
 
-const PersonQueryResults = ({queryParams}) => {
+const PersonQueryResults = ({queryParams, select, handleSelect, exclude}) => {
     console.log(queryParams);
 
     return (
@@ -67,6 +86,9 @@ const PersonQueryResults = ({queryParams}) => {
                 orcid: queryParams.orcid || null, 
                 groups: queryParams.groups.length === 0 ? [publicGroupID] : queryParams.groups, 
             }}
+            select={select}
+            handleSelect={handleSelect}
+            exclude={exclude}
         />
     );
 };
