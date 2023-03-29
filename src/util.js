8f1b48dbd79c683e50b9e4817d5cd24e24f0fdf5
Douglas Meredith <noisyflowers@douglasmeredith.net>
2023-03-29 16:43:58 -0700

    In sort routine, allow second sort param to be numeric, indicated by prefixed "#"

--- src/util.js
+++ src/util.js
@@ -13,13 +13,22 @@ export const alphabetize = (list, sortField) => {
 }
 
 export const sort = (list, sortField1, sortField2) => {
+    console.log("sort")
+    console.log(list);
+
     return list.sort((a,b) => {
-        //"z" forces null/blank names to end of list
+        //"z"/1000 forces null/blank/NaN to end of list
         const field1A = a[sortField1] ? a[sortField1].toString().toUpperCase() : "z"; 
         const field1B = b[sortField1] ? b[sortField1].toString().toUpperCase() : "z"; 
 
-        const field2A = a[sortField2] ? a[sortField2].toString().toUpperCase() : "z"; 
-        const field2B = b[sortField2] ? b[sortField2].toString().toUpperCase() : "z"; 
+        let field2A, field2B;
+        if (sortField2.startsWith("#")) {
+            field2A = a[sortField2.substring(1)] ? Number.parseFloat(a[sortField2.substring(1)]) : 1000; 
+            field2B = b[sortField2.substring(1)] ? Number.parseFloat(b[sortField2.substring(1)]) : 1000; 
+        } else {
+            field2A = a[sortField2] ? a[sortField2].toString().toUpperCase() : "z";
+            field2B = b[sortField2] ? b[sortField2].toString().toUpperCase() : "z";
+        }
 
         if (field1A < field1B) {
             return -1;
