88a5edc0ac44c484704aac4c43d73f059d96e347
Douglas Meredith <noisyflowers@douglasmeredith.net>
2023-04-20 17:40:25 -0700

    Remove Accordion style overrides

--- src/index.js
+++ src/index.js
@@ -263,15 +263,6 @@ const theme = createTheme({
             
         }
       },
-      MuiAccordionSummary: {
-        styleOverrides: {
-            root: {
-                  backgroundColor: "rgba(0,0,0,.05)",
-            }
-            
-        }
-      },
-
     }
 });
 
