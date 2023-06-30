b759161f1ff0e6ba81abe8793af9162430c9525c
Douglas Meredith <noisyflowers@douglasmeredith.net>
2023-06-30 12:50:36 -0700

    Set author init value in onChange

--- src/components/Reference/PublicationTypeSelect.js
+++ src/components/Reference/PublicationTypeSelect.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MenuItem } from "@mui/material";
-import { Field } from "formik";
+import { Field, useFormikContext } from "formik";
 import { publicationTypes } from "../../Lists.js"
 import { TextField } from 'formik-mui';
 
 export const PublicationTypeSelect = (props) => {
+
+    const formikProps = useFormikContext()
+
     const style = {minWidth: "12ch"}
     return (
         <Field
@@ -17,12 +20,33 @@ export const PublicationTypeSelect = (props) => {
             SelectProps={{
                 multiple: false,
             }}
+            onChange={event => {
+                //This onChange is to initials values.authors correctly for the publicationType.
+                //I don't like that these are hard-coded while we are pulling the values from a 
+                //separate list. Should probably make that a list of objects with author init value
+                //field in each or something.
+                console.log(event)
+                if (event.target.value === "journal article" || 
+                    event.target.value === "standalone book" || 
+                    event.target.value === "contributed article in edited book" ||
+                    event.target.value === "unpublished") {
+                    formikProps.values.authors = [{
+                        pbotID: '',
+                        order:'',
+                        searchName:'',
+                    }]
+                } else {
+                    formikProps.values.authors = []
+                }
+                formikProps.handleChange(event);
+            }}
             disabled={false}
         >
             {publicationTypes.map((pt) => (
                 <MenuItem 
                     key={pt} 
                     value={pt}
+                    authorrequired="true"
                 >{pt}</MenuItem>
             ))}
         </Field>
