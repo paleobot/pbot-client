2e9bbc19efbd63df91285619552b4dcbe7bcbec7
Douglas Meredith <noisyflowers@douglasmeredith.net>
2023-07-27 13:49:48 -0700

    Switch to using sort instead of alphabetize. To that end, change massage to not prepend order.

--- src/components/CharacterInstance/CharacterInstances.js
+++ src/components/CharacterInstance/CharacterInstances.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { alphabetize } from '../../util.js';
+import { sort } from '../../util.js';
 
 const massage = cI => {
-    cI.sortName = `
-            ${cI.state.order !== null ? `${cI.state.order}` : ``}${cI.character.name}${cI.state.value !== null ? `${cI.state.value}` : `${cI.state.State.name}`}
-`.toUpperCase();
+    cI.sortName = `${cI.character.name}${cI.state.value !== null ? cI.state.value : cI.state.State.name}`.toUpperCase();
 return cI
 };
 
@@ -13,7 +11,7 @@ function CharacterInstances(props) {
     if (!props.characterInstances) return ''; //TODO: is this the best place to handle this?
     //console.log(props.characterInstances);
 
-    let characterInstances = alphabetize([...props.characterInstances].map(cI => massage({...cI})), "sortName");
+    let characterInstances = sort([...props.characterInstances].map(cI => massage({...cI})), "#order", "sortName");
     
     const style = {marginLeft:"4em"}
     return characterInstances.map(({pbotID, character, state}) => (
