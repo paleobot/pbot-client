6ccb7a51858376bd36e868b1e1aa923df7c2e983
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-04-16 16:56:26 -0700

    Remove graphql stuff

--- src/components/Group/GroupSelect.js
+++ src/components/Group/GroupSelect.js
@@ -3,39 +3,8 @@ import { Field } from 'formik';
 import { MenuItem } from '@mui/material';
 import { TextField } from 'formik-mui';
 import { alphabetize } from '../../util.js';
-import {
-  useQuery,
-  gql
-} from "@apollo/client";
-
 export const GroupSelect = (props) => {
     console.log("GroupSelect");
-
-    const gQL = gql`
-            query {
-                Group {
-                    pbotID
-                    name
-                }            
-            }
-        `;
-
-    const { loading: loading, error: error, data: data } = useQuery(gQL, {fetchPolicy: "cache-and-network"});
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
-                                 
-    console.log(data.Group);
-    
-    
-    const groups = alphabetize(
-        props.omitPublic ?
-            [...data.Group].filter(group => "public" !== group.name) :
-            [...data.Group],
-        "name"
-    );
-    console.log(groups)
-    
     return (
         <Field
             component={TextField}
