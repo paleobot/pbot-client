72cb6d4317eb0e3ee0a665c615ded129b95c5f78
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-04-29 17:12:25 -0700

    Moved from ColletionMutateForm into its own file

---
+++ src/components/util/TextFieldController.jsx
@@ -0,0 +1,48 @@
+import { TextField } from "@mui/material";
+import { Controller } from "react-hook-form";
+
+
+export const TextFieldController = ({name, label, control, errors, ...props}) => {
+    const pathElements = name.split(".");
+
+    let topName, index, fieldName;
+    if (pathElements.length === 3) {
+        topName = pathElements[0];
+        index = pathElements[1];
+        fieldName = pathElements[2];
+    } else if (pathElements.length === 2) {
+        topName = pathElements[0];
+        fieldName = pathElements[1];          
+    } 
+
+    return (
+        <Controller
+            control={control}
+            render={({ field }) => <TextField 
+                {...field} 
+                {...props}
+                sx={[
+                    {minWidth: "12ch"},
+                    // You cannot spread `sx` directly because `SxProps` (typeof sx) can be an array.
+                    ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
+                ]}
+                label={label}
+                error={topName ?
+                    index ? 
+                        !!errors[topName]?.[index]?.[fieldName] :
+                        !!errors[topName]?.[fieldName] :
+                    !!errors[name]
+                } 
+                helperText={topName ?
+                    index ?
+                        errors[topName]?.[index]?.[fieldName]?.message :
+                        errors[topName]?.[fieldName]?.message :
+                    errors[name]?.message
+                }
+            />}
+            name={name}
+            //style={{minWidth: "12ch", width:"100%"}}
+            disabled={false}
+        />
+    );
+}
