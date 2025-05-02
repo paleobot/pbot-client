ebff0eed47ddaf7854ff777e4c58e3c9b289505c
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-05-01 18:25:36 -0700

    Use IIFE for file rendering.  Currently only on documents. Need to move to its own component and use for all file types.

--- src/components/Collection/CollectionMutateForm.js
+++ src/components/Collection/CollectionMutateForm.js
@@ -211,7 +211,7 @@ const fileShape = {
 const FileFields = ({name, index, control, errors}) => {
     return (
         <Stack direction="column" spacing={0} sx={{ marginLeft:"1.5em"}}>
-            <FileSelectController name={`${name}.${index}.name`} control={control} errors={errors} sx={{width:"75%"}}/>
+            <FileSelectController name={`${name}.${index}.name`} control={control} errors={errors} sx={{width:"75%",}}/>
         </Stack>
     )
 }    
@@ -512,6 +512,51 @@ const CollectionMutateForm = ({handleSubmit: hSubmit, mode}) => {
                                     </TabPanel>
                                     <TabPanel value="2">
 
+                                        {(() => {
+                                            if (
+                                                    mode === "edit" && 
+                                                    control._formValues.files && 
+                                                    control._formValues.files.filter(file => file.type === "documents").length > 0
+                                            ) {
+                                                return (
+                                                    <>
+                                                    <InputLabel sx={{ marginTop:"1.5em", }}>
+                                                        Documents
+                                                    </InputLabel>
+                                                    <InputLabel sx={{ marginTop:"1em", marginBottom:"0.5em", marginLeft:"1em"}}>
+                                                        Existing (check to delete)
+                                                    </InputLabel>
+                                                    {control._formValues.files.map((file, index) => {
+                                                        if (file.type === "documents") {
+                                                            return (
+                                                                <Stack direction="row" alignItems="center" spacing={0} sx={{ marginLeft:"2em"}}>
+                                                                    <Typography name={`files.${index}.name`}  control={control} errors={errors} sx={{width:"50%", }} variant ="body2">
+                                                                        {file.name}
+                                                                    </Typography>
+                                                                    <CheckboxController sx={{padding:"0", margin:"0"}} name={`files.${index}.delete`} control={control} errors={errors} />
+                                                                </Stack>
+                                                            )
+                                                        }
+                                                        return null;
+                                                    })}
+
+                                                    <MultiManager label="New" name="documents" content={FileFields} shape={fileShape} control={control} watch={watch("documents")} errors={errors} optional sx={{marginLeft: "1em"}} />
+                                                    <br />
+
+                                                    </>
+            
+                                                )
+                                            } else {
+                                                return (
+                                                    <>
+                                                    <MultiManager label="Documents" name="documents" content={FileFields} shape={fileShape} control={control} watch={watch("documents")} errors={errors} optional/>
+                                                    <br />
+                                                    </>
+                                                )
+                                            }
+                                        })()}
+
+                                        {/*
                                         {//TODO: This is to display existing files when editing. Needs to be moved into its own component.
                                         }
                                         {mode === "edit" && control._formValues.files && control._formValues.files.length > 0 &&
@@ -535,9 +580,10 @@ const CollectionMutateForm = ({handleSubmit: hSubmit, mode}) => {
 
                                         </>
                                         }
-                                        
+
                                         <MultiManager label="Documents" name="documents" content={FileFields} shape={fileShape} control={control} watch={watch("documents")} errors={errors} optional/>
                                         <br />
+                                        */}
 
                                         <MultiManager label="Images" name="images" content={FileFields} shape={fileShape} control={control} watch={watch("images")} errors={errors} optional/>
                                         <br />
