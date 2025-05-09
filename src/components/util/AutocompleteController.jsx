8b579b85d7b4803970df89fadd814696976d2ce2
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-05-08 17:45:41 -0700

    Add onInputChange to catch arbitrary text entry.  A way better way to get at the error/helperText.

--- src/components/util/AutocompleteController.jsx
+++ src/components/util/AutocompleteController.jsx
@@ -3,9 +3,10 @@ import { Controller } from "react-hook-form";
 
 
 export const AutocompleteController = ({name, label, control, errors, ...props}) => {
-    const pathElements = name.split(".");
 
+    /*
     //This, with the use of eval, provides a clever, if inelegant, way to handle the error and helperText props below for arbitrarily nested fields
+    const pathElements = name.split(".");
     const errorsPathString = pathElements.reduce((acc, curr, idx) => {
         if (idx === 0) {
             return `${acc}${curr}`;
@@ -17,16 +18,19 @@ export const AutocompleteController = ({name, label, control, errors, ...props})
             }
         }    
     },'errors.')
-
+    */
     return (
         <Controller
             control={control}
-            render={({ field }) =>  <Autocomplete
+            render={({ field, fieldState }) =>  <Autocomplete
                 {...field} 
                 freeSolo
-                options={["UA Library", "test"]}
+                options={["UA Library"]}
                 getOptionLabel={(option) => option}
                 onChange={(_, value) => field.onChange(value)}
+                onInputChange={(_, value) => {
+                    if (value) field.onChange(value);
+                }}
                 renderInput={(params) => <TextField 
                     {...params} 
                     {...props}
@@ -36,8 +40,10 @@ export const AutocompleteController = ({name, label, control, errors, ...props})
                         ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
                     ]}
                     label={label}
-                    error={eval(`!!${errorsPathString}`)} 
-                    helperText={eval(`${errorsPathString}?.message`)}
+                    //error={eval(`!!${errorsPathString}`)} 
+                    //helperText={eval(`${errorsPathString}?.message`)}
+                    error={fieldState.invalid} 
+                    helperText={fieldState.error?.message}
                 />}
             />}
             name={name}
