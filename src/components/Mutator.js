e3c0169b0dd9c91ec69be336ff75662691c59719
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-05-09 18:28:48 -0700

    Changes to make it work with azlibrary api

--- src/components/Mutator.js
+++ src/components/Mutator.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Mutator = (props) => {
     console.log("Mutator");
@@ -10,13 +11,88 @@ const Mutator = (props) => {
     let [data, setData] = useState(null)
     let [error, setError] = useState(null)
 
+    //const url = "http://localhost:3000/api/v1/";
+    //const url = new URL(`${props.entity}${props.id ? `/${props.id}` : ''}`, process.env.REACT_APP_AZLIB_API_URL)
+
+    //TODO: Get this properly
+    //This is for localhost
+    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOnsidXNlcl9pZCI6NSwiZW1haWwiOiJlbWFpbEBlbWFpbC5jb20iLCJyb2xlX2lkIjoxLCJmaXJzdF9uYW1lIjoiUGFzcyIsImxhc3RfbmFtZSI6IldvcmQiLCJvcmdhbml6YXRpb24iOiJub25lIiwidG9zX2FjY2VwdGVkIjp0cnVlLCJhcHByb3ZlZCI6dHJ1ZSwiY3JlYXRlZF9kYXRlIjoiMjAyMC0wMi0xNFQyMzo0NjoxMi40NjJaIiwibW9kaWZpZWRfZGF0ZSI6bnVsbCwicHdfcmVzZXRfdG9rZW4iOm51bGwsInB3X3Jlc2V0X3RpbWUiOm51bGx9LCJleHAiOjE2OTkzOTY1NTY0OTZ9.m0SljTDFdW_8fpM2dgdQAEz23h4oOlRrFoyDP1x-6as" 
+
     useEffect(() => {
 
         async function fetchData () {
-            const url = "http://localhost:3000/api/v1/";
             try {
-                const response = await fetch(url);
-                console.log(response)
+                let response
+                if (props.mode === "replace") {
+                    console.log("PUTing");
+                    response = await fetch(
+                        `${process.env.REACT_APP_AZLIB_API_URL}/api/v1/${props.entity}/${props.id}`,
+                        {
+                            method: 'PUT',
+                            headers: {
+                                'Content-Type': 'multipart/form-data',
+                                'Accept': 'application/json',
+                                'Authorization': "Bearer " + token
+                            },
+                            body: props.formData,
+                        }
+                    );                
+                } else if (props.mode === "edit") {
+                    console.log("PATCHing");
+                    response = await fetch(
+                        `${process.env.REACT_APP_AZLIB_API_URL}/api/v1/${props.entity}/${props.id}`,
+                        {
+                            method: 'PATCH',
+                            headers: {
+                                'Content-Type': 'multipart/form-data',
+                                'Accept': 'application/json',
+                                'Authorization': "Bearer " + token
+                            },
+                            body: props.formData,
+                        }
+                    );                
+                } else if (props.mode === "delete") {
+                    console.log("DELETing");
+                    response = await fetch(
+                        `${process.env.REACT_APP_AZLIB_API_URL}/api/v1/${props.entity}/${props.id}`,
+                        {
+                            method: 'DELETE',
+                            headers: {
+                                'Accept': 'application/json',
+                                'Authorization': "Bearer " + token
+                            },
+                        }
+                    );                
+                } else {
+                    console.log("POSTing");
+                    
+                    response = await fetch(
+                        `${process.env.REACT_APP_AZLIB_API_URL}/api/v1/${props.entity}`,
+                        {
+                            method: 'POST',
+                            headers: {
+                                //'Content-Type': 'multipart/form-data',
+                                'Accept': 'application/json',
+                                'Authorization': "Bearer " + token
+                            },
+                            body: props.data,
+                        }
+                    );    
+                    
+                    /*
+                    Had this for debugging, but it didn't help. The problem appears to be in FileSelectController. Ghosting for now
+                    response = await axios.post(`${process.env.REACT_APP_AZLIB_API_URL}/api/v1/${props.entity}`, props.data, {
+                        headers: {
+                            'Content-Type':'multipart/form-data',	
+                            'Accept': 'application/json',
+                            'Authorization': "Bearer " + token
+                        }
+                    });
+                    */
+
+                }
+                console.log("response");
+                
                 if (!response.ok) {
                     throw new Error(`Response status: ${response.status}`);
                 }
@@ -52,10 +128,13 @@ const Mutator = (props) => {
         const style = {textAlign: "left", width: "100%", margin: "auto", marginTop:"1em"};
         return props.mode === "edit" ?
         (
-            <div key={data[`Update${props.entity}`][`${entityID}`]} style={style}>
-                {data[`Update${props.entity}`][`${entityID}`]} updated<br />
-                <br />
+            <div>
+                {JSON.stringify(data)}
             </div>
+            //<div key={data[`Update${props.entity}`][`${entityID}`]} style={style}>
+            //    {data[`Update${props.entity}`][`${entityID}`]} updated<br />
+            //    <br />
+            //</div>
         ) :
         props.mode === "create" ?
         (
@@ -69,10 +148,13 @@ const Mutator = (props) => {
         ) :
         props.mode === "delete" ?
         (
-            <div key={data[`Delete${props.entity}`][`${entityID}`]} style={style}>
-                {data[`Delete${props.entity}`][`${entityID}`]} deleted<br />
-                <br />
+            <div>
+                {JSON.stringify(data)}
             </div>
+            //<div key={data[`Delete${props.entity}`][`${entityID}`]} style={style}>
+            //    {data[`Delete${props.entity}`][`${entityID}`]} deleted<br />
+            //    <br />
+            //</div>
         ) :
         '';
                 
