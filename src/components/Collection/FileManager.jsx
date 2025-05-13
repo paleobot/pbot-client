334256d240185f6ec117ed18c84b1917f2c23f54
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-05-12 17:26:12 -0700

    Assign file type based on name parameter

--- src/components/Collection/FileManager.jsx
+++ src/components/Collection/FileManager.jsx
@@ -15,13 +15,28 @@ const FileFields = ({name, index, control, errors}) => {
 }  
 
 export const FileManager = ({ name, label, control, watch, errors, mode }) => {
+    console.log("FileManager");
+    console.log(JSON.parse(JSON.stringify(control._formValues)));
+
+    const fileType = 
+        name === "documentFiles" ? "documents" :
+        name === "imageFiles" ? "images" :
+        name === "noteFiles" ? "notes:misc" :
+        name === "metadataFiles" ? "metadata" :
+        name === "gems2Files" ? "gisdata:gems2" :
+        name === "ncgmp09Files" ? "gisdata:ncgmp09" : 
+        name === "legacyFiles" ? "gisdata:legacy" :
+        name === "layerFiles" ? "gisdata:layers" :
+        name === "rasterFiles" ? "gisdata:raster" : 
+        null;
+
     return (
         <>
         {(() => {
             if (
                     mode === "edit" && 
                     control._formValues.metadata.files && 
-                    control._formValues.metadata.files.filter(file => file.type === name).length > 0
+                    control._formValues.metadata.files.filter(file => file.type === fileType).length > 0
             ) {
                 return (
                     <>
@@ -32,7 +47,7 @@ export const FileManager = ({ name, label, control, watch, errors, mode }) => {
                         Existing (check to delete)
                     </InputLabel>
                     {control._formValues.metadata.files.map((file, index) => {
-                        if (file.type === name) {
+                        if (file.type === fileType) {
                             return (
                                 <Stack direction="row" alignItems="center" spacing={0} sx={{ marginLeft:"2em"}}>
                                     <Typography name={`metadata.files.${index}.name`}  control={control} errors={errors} sx={{width:"50%", }} variant ="body2">
