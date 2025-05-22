5743cebf2473db99b125729f65497afd2cdd65ca
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-05-21 17:44:22 -0700

    Moved SelectController to own file

--- src/components/Collection/CollectionMutateForm.js
+++ src/components/Collection/CollectionMutateForm.js
@@ -13,6 +13,7 @@ import { AutocompleteController } from '../util/AutocompleteController.jsx';
 import { TextFieldController } from '../util/TextFieldController';
 import { ExistingCollectionManager } from './ExistingCollectionManager';
 import { FileManager } from './FileManager.jsx';
+import { SelectController } from '../util/SelectController.jsx';
 
 const LabeledCheckboxController = ({name, label, control, errors, ...props}) => {
     //Note 1: We could add path elements here to allow for nested checkboxes, but we don't need it right now. If we do, we can use the same logic as in TextFieldController, or better yet, we could factor it out into a common controller function.
@@ -39,84 +40,6 @@ const LabeledCheckboxController = ({name, label, control, errors, ...props}) => 
     )
 
 }                         
-
-const SelectController = ({name, label, options, control, errors, ...props}) => {
-    //Note 1: We could add path elements here to allow for nested checkboxes, but we don't need it right now. If we do, we can use the same logic as in TextFieldController, or better yet, we could factor it out into a common controller function.
-
-    const [data, setData] = React.useState(null);
-    const [loading, setLoading] = React.useState(true);
-    const [error, setError] = React.useState(null);
-  
-    React.useEffect(() => {
-        const fetchData = async () => {
-            //If options is an array, we don't need to fetch anything. Just set the data.
-            if (Array.isArray(options)) {
-                setData(options);
-                setLoading(false);
-                return;
-            }
-
-            //Otherwise, we need to fetch the data. 
-            //TODO: For now, I'm assuming options is a URL. I might change that to expect only the path, and then add the base URL in the fetchData function.
-            try {
-                console.log(`Fetching data from ${process.env.REACT_APP_AZLIB_API_URL}`);
-                const response = await fetch(new URL(options.path, process.env.REACT_APP_AZLIB_API_URL));
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                let json = await response.json();
-                json = json.data.map((item) => {
-                    return {
-                        name: item[options.nameField],
-                        value: item[options.valueField]
-                    }
-                })
-                setData(json);
-                setLoading(false);
-            } catch (e) {
-                setError(e);
-                setLoading(false);
-            }
-        }
-
-        fetchData();
-    }, []);
-  
-    if (loading) {
-        return <Typography variant="body1">Loading...</Typography>;
-    }
-
-    if (error) {
-        return <Typography color="error" variant="caption">Error accessing API: {error.message}</Typography>;
-    }
-
-  
-    return (
-        <Controller
-            control={control}
-            name={name}
-            render={({ field, fieldState }) => 
-                <TextField
-                    {...field}
-                    {...props}
-                    select
-                    sx={[
-                        {minWidth: "12ch"},
-                        // You cannot spread `sx` directly because `SxProps` (typeof sx) can be an array.
-                        ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
-                    ]}
-                    label={label}
-                    error={fieldState.invalid} 
-                    helperText={fieldState.error?.message}
-                >
-                    {data.map((option, index) => (
-                        <MenuItem value={option.value}>{option.name}</MenuItem>
-                    ))}
-                </TextField>
-            }
-        />
-    )
-}
 
 const authorShape = {
     person: '',
@@ -351,7 +274,7 @@ const CollectionMutateForm = ({handleSubmit: hSubmit, mode}) => {
     //To clear form when mode changes 
     React.useEffect(() => {
             resetForm();
-        },[mode]);
+    },[mode]);
     
     const [selectedTab, setSelectedTab] = React.useState('1');
     const handleTabChange = (event, newValue) => {
