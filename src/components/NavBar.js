23c4562e995b29c3197cca47640f692ebe410a29
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-05-21 17:45:35 -0700

    Add explore option

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -66,7 +66,14 @@ export default function NavBar() {
             <Toolbar />
             <Divider />
             <List>
-                <ListItem key="Workbench" disablePadding>
+                <ListItem key="Query" disablePadding>
+                    <ListItemButton onClick={() => {handleDrawerToggle(); navigate(`/query`);}}>
+                        <ListItemIcon>
+                            <ManageSearchIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary="Explore" />
+                    </ListItemButton>
+                </ListItem>                <ListItem key="Workbench" disablePadding>
                     <ListItemButton onClick={() => {handleDrawerToggle(); navigate(`/mutate`);}}>
                         <ListItemIcon>
                             <EngineeringIcon/>
