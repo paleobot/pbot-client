4bde164cc4482f3c91fa6e06302e1dd6a3b9f4d4
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-05-29 17:46:10 -0700

    Move LabeledCheckboxController into its own file

--- src/components/Collection/CollectionMutateForm.js
+++ src/components/Collection/CollectionMutateForm.js
@@ -14,32 +14,7 @@ import { TextFieldController } from '../util/TextFieldController';
 import { ExistingCollectionManager } from './ExistingCollectionManager';
 import { FileManager } from './FileManager.jsx';
 import { SelectController } from '../util/SelectController.jsx';
-
-const LabeledCheckboxController = ({name, label, control, errors, ...props}) => {
-    //Note 1: We could add path elements here to allow for nested checkboxes, but we don't need it right now. If we do, we can use the same logic as in TextFieldController, or better yet, we could factor it out into a common controller function.
-
-    return (
-        <Controller
-            control={control}
-            render={({ field }) => <FormControlLabel 
-                label={label}        
-                sx={{marginTop: "1.5em"}}
-                control={
-                    <Checkbox
-                        {...field} 
-                        {...props}
-                        checked={field.value}
-                        onChange={(e) => field.onChange(e.target.checked)}
-                    />
-               }
-            />}
-            error={!!errors[name]} 
-            helperText={errors[name]?.message}    
-            name={name}
-        />   
-    )
-
-}                         
+import { LabeledCheckboxController } from '../util/LabeledCheckboxController.jsx';
 
 const authorShape = {
     person: '',
