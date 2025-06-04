26ad2977da993385255b27453f8f4538000bb961
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-06-04 13:33:40 -0700

    Clearing out PBot cruft

--- src/components/Account.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Account = () => (
-    <p>Account not yet implemented</p>
-);
-
-export default Account;
--- src/components/Resources.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Resources = () => (
-    <p>Resources not yet implemented</p>
-);
-
-export default Resources;
