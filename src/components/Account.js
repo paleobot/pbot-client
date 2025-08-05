e9d0235baa658a24e1a171cc3a829fff54fae4d6
Douglas Meredith <noisyflowers@douglasmeredith.net>
2025-08-05 16:46:58 -0700

    Add expanded text

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Account = () => (
-    <p>Account not yet implemented</p>
+    <p>Full user-directed account functionality has not been implemented yet. Please contact <a href="mailto:azaffos@arizona.edu">Andrew Zaffos</a> for any needed changes to your account such as resetting your password or changing your email.</p>
 );
 
 export default Account;
